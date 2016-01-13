8511df62754a6be669a5fdd56ba94f63ed484c83
Eric Ma <ericmajinglong@gmail.com>
2016-01-13 10:16:11 -0500

    condensed update_github

--- update_github.py
+++ update_github.py
@@ -1,30 +1,22 @@
 import os
 
+def update_directory(dir):
+    print('Updating {dir}...'.format(dir=dir))
+    os.chdir(dir)
+    status = os.system('git pull')
+    if status == 0:
+        print('Succeeded.')
+    else:
+        print('Failed')
 
 # Update picamera-auto
-os.chdir('/home/pi/github/picamera-auto')
-status = os.system('git pull')
-if status == 0:
-    print('Update on picamera-auto succeeded.')
-else:
-    print('Update on picamera-auto failed.')
+update_directory('/home/pi/github/picamera-auto')
 
 # Update bluetooth-proximity-tracker
-os.chdir('/home/pi/github/bluetooth-proximity-tracker')
-status = os.system('git pull')
-if status == 0:
-    print('Update on bluetooth-proximity-tracker succeeded.')
-else:
-    print('Update on bluetooth-proximity-tracker failed.')
-
+update_directory('/home/pi/github/bluetooth-proximity-tracker')
 
 # Update configuration files
-os.chdir('/home/pi/github/tikicam-config')
-status = os.system('git pull')
-if status == 0:
-    print('Update on tikicam-config succeeded.')
-else:
-    print('Update on tikicam-config failed.')
+update_directory('/home/pi/github/tikicam-config')
 
 # Copy wpa_supplicant configuration file.
 os.chdir('/home/pi/github/tikicam-config')
